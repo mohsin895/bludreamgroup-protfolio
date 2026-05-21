1dc94ed0d8f8d954056a610ca329af3799920a1e
Essential-Infotech <33011566+mohsin895@users.noreply.github.com>
2026-05-21 10:28:50 +0600

    dsfdrsefr

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "bluedreamprotfolio.nelsistech.com",
-        pathname: "/**",
-      },
-    ],
-  },
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "bluedreamprotfolio.nelsistech.com",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+// };
+//
+// module.exports = nextConfig;
+import type { NextConfig } from "next";
+
+const config: NextConfig = {
+    images: {
+        unoptimized: process.env.NODE_ENV === 'development',
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'admin.shopnillchowdhury.com',
+                pathname: '/**',
+            },
+        ],
+    },
 };
 
-module.exports = nextConfig;
+export default config;
